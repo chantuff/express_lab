1176e08c9a3c637e1398f2789bd87aa48b8cfdbf
chantuff <chan.tuff@yahoo.com>
2023-12-06 15:33:29 -0500

    Magic 8 ball working

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require('express');
 const app = express();
   // Mount routes
 
+  const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
   app.get('/', function(req, res) {
     res.send('Hello World!');
 })
@@ -17,6 +19,18 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 	console.log(req.params);
 	res.send('TIP:' + req.params.total * req.params.tipPercentage/100);
 });
+
+app.get('/magic/:question', (req, res) => {
+  // Get the question from the URL parameters
+  const question = req.params.question;
+
+  // Get a random response from the array
+  const randomResponse = responses[Math.floor(Math.random() * responses.length)];
+
+  // Send the response 
+  res.send(`Question: ${question}<h1>\nAnswer: ${randomResponse}</h1>`);
+});
+
 
 // Tell the app to listen on port 3000
 app.listen(3000, function() {
